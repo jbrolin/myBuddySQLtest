5156c99d04a30684d739692a03b2b5deb9841a4b
jbrolin <brolin.jonas@gmail.com>
2018-03-01 16:34:29 +0100

    ett

--- README.md
+++ README.md
@@ -3,9 +3,8 @@
 This README would normally document whatever steps are necessary to get your application up and running.
 
 ### What is this repository for? ###
-
-* Quick summary
-* Version
+* Mybuddy project is to have a db....
+* Version 2
 * [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
 
 ### How do I get set up? ###
--- test.php
+++ test.php
@@ -1,3 +1,26 @@
-$sql = "SELECT `userdata`.`name`\n"
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "tintin10";
+$dbname = "mybuddy";
 
-    . "FROM `userdata`";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT name, age, FROM userdata";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "name: " . $row["name"]. " - Age: " . $row["age"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
