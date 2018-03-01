16d07abfffcb3fa993e177e07c0a869fd101bfc2
jbrolin <brolin.jonas@gmail.com>
2018-03-01 17:04:18 +0100

    two

--- test.php
+++ test.php
@@ -1,26 +1,40 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "tintin10";
-$dbname = "mybuddy";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "mybuddy");
+ 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT name, age, FROM userdata";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "name: " . $row["name"]. " - Age: " . $row["age"]. "<br>";
-    }
-} else {
-    echo "0 results";
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$conn->close();
+ 
+// Attempt select query execution
+$sql = "SELECT * FROM userdata";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>name</th>";
+                echo "<th>age</th>";
+                echo "<th>email</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['age'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
 ?>
